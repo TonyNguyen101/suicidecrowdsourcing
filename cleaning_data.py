2939c4d7b3d810d5ba6610432ed4b9fd9b15c851
miahunsicker <hunsickermia@gmail.com>
2016-04-15 17:07:28 -0700

    ..useful for model...

--- cleaning_data.py
+++ cleaning_data.py
@@ -3,6 +3,7 @@ import datetime
 
 
 def get_json_df(datafile):
+    ''''fix trailing data...?'''
     with open(datafile, 'rb') as f:
         data = f.readlines()
 
@@ -11,7 +12,16 @@ def get_json_df(datafile):
     data_df = pd.read_json(data_json_str)
     return data_df
 
+
+def filter_tweets(string):
+    '''remove any mentions'''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",string).split())
+
 if __name__ == '__main__':
     df = get_json_df('suicide_tweets.json')
     df2 = df[['id','user','text']]
+
+    '''make data set smaller so we can pass
+    through to the FE in case DB's dont work'''
+
     df2.to_pickle('data/%d_tweets.pkl') %(str(datetime.datetime.now().date))
